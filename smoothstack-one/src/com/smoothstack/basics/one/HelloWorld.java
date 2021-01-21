e88f7fc9c71e1ef3e96b40185e0fb349f3e21c62
mvance-smoothstack <77799438+mvance-smoothstack@users.noreply.github.com>
2021-01-21 12:34:41 -0500

    first local commit

---
+++ smoothstack-one/src/com/smoothstack/basics/one/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.smoothstack.basics.one;
+
+/**
+ * @author max
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
