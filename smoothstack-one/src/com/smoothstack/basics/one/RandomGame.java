6049e4c546a95aba57842bb900191dabcadc112b
mvance-smoothstack <77799438+mvance-smoothstack@users.noreply.github.com>
2021-01-21 14:07:18 -0500

    added some comments

--- smoothstack-one/src/com/smoothstack/basics/one/RandomGame.java
+++ smoothstack-one/src/com/smoothstack/basics/one/RandomGame.java
@@ -19,14 +19,14 @@ public class RandomGame {
 		int theNumber;
 		Random randomGenerator = new Random();
 		Scanner input = new Scanner(System.in);
-		theNumber = randomGenerator.nextInt(100) + 1;
+		theNumber = randomGenerator.nextInt(100) + 1;	//the number the player is trying to guess
 		int guess = 0;
-		final int TRIES = 5;
+		final int TRIES = 5;	//number of attempts the player gets
 		
 		for (int x = 0; x < TRIES; x++) {
 			System.out.println("What is your guess? Input a number between 1-100.");
 			guess = input.nextInt();
-			if ((guess > (theNumber - 10) && guess < (theNumber + 10))) {
+			if ((guess > (theNumber - 10) && guess < (theNumber + 10))) {		//if the guess was within 10 of the number, win
 				System.out.println("You win!");
 				break;
 			} else {
@@ -36,9 +36,9 @@ public class RandomGame {
 				}
 			}
 		}
-		System.out.print("The number was " + theNumber + ".");
+		System.out.print("The number was " + theNumber + ".");	//whether the player won or lost, tell the number
 		
-		input.close();
+		input.close();		//and close the scanner at the end
 	}
 
 }
